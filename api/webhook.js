dfb777202ebfd2bfab7b30f8555b5b2c3f6f0d1c
nourhighbase <nour@highbaseco.com>
2025-04-29 13:50:47 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN; // now using your env variable
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
   if (req.method === 'GET') {
     const mode = req.query['hub.mode'];
@@ -7,12 +7,22 @@ export default function handler(req, res) {
     const challenge = req.query['hub.challenge'];
 
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      res.status(200).send(challenge);
+      return res.status(200).send(challenge);
     } else {
-      res.sendStatus(403);
+      return res.sendStatus(403);
     }
-  } else {
-    console.log("Received webhook event:", req.body);
-    res.sendStatus(200);
   }
+
+  if (req.method === 'POST') {
+    // Make sure it won't crash if body is undefined
+    try {
+      console.log("Received webhook event:", req.body || "No body received");
+      return res.sendStatus(200);
+    } catch (err) {
+      console.error("POST error:", err);
+      return res.sendStatus(500);
+    }
+  }
+
+  res.sendStatus(405); // method not allowed
 }
