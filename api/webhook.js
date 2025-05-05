4d00bd7cc7d59e95d2f8141d34d53e62273c8ea1
nourhighbase <nour@highbaseco.com>
2025-05-05 11:52:06 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
           console.log("✅ Webhook event:", parsedBody);
           res.status(200).send("EVENT_RECEIVED");
         } catch (err) {
-          console.error("❌ JSON Error:", err);
+          console.error("❌ JSON parse error:", err);
           res.status(400).send("Invalid JSON");
         }
       });
